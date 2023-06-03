bf1f97cb7595b0e2b26f8aeb556efb14c45d2f40
Manuga232 <52424599+Manuga232@users.noreply.github.com>
2023-06-03 00:19:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,43 @@
 from flask import Flask, request
-
 import util
 
- 
-
-app= Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/welcome', methods=['GET'])
-
 def index():
-
     return "welcome developer"
 
- 
-
 @app.route('/whatsapp', methods=['GET'])
-
 def VerifyToken():
-
- 
-
     try:
-
         accessToken = "777FCDS4DS8F777FD"
-
         token = request.args.get("hub.verify_token")
-
         challenge = request.args.get("hub.challenge")
 
- 
-
         if token != None and challenge != None and token == accessToken:
-
             return challenge
-
         else:
-
             return "", 400
-
     except:
-
         return "", 400
 
- 
-
 @app.route('/whatsapp', methods=['POST'])
-
 def ReceivedMessage():
-
- 
-
     try:
-
         body = request.get_json()
+        entry = body["entry"][0]
+        changes = entry["changes"][0]
+        value = changes["value"]
+        message = (value["messages"])[0]
+        number = message["from"]
 
-        entry = (body["entry"])[0]
-
-        changes  = (entry["changes"])[0]
-
-        value = changes["values"]
-
-        message = (value["message"])[0]
-
-        number =  message["from"]
- 
 
         text = util.GetTextUser(message)
+        print(text)
 
-        print(message)
-        
+        return "EVENT_RECEIVED"
+    except:
         return "EVENT_RECEIVED"
 
-    except:
-
-        return "EVENT_RECEIVED"    
-
-if(__name__ == "__main__"):
-
+if __name__ == "__main__":
+    app.run()
